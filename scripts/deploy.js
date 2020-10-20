d7fe3e61781be4c5f6ce258bdfe07a533e086cab
Nipol <ys.choi@me.com>
2020-10-20 14:34:44 +0900

    deployable

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,39 +4,34 @@ async function main() {
   const SCAMMTemplate = await ethers.getContractFactory('SCAMM');
 
   // 1. Token 배포
-  // 0xaa13a4C86C410f97756fE0d76d07FB644feB2799
   const scaleToken = await ScaleTokenTemplate.deploy();
   await scaleToken.deployed();
-  // 0x2f2d1790ae082d1847e9c4eefb58bd311f6ff886
-  // const yUSDA = await StandardTokenTemplate.deploy();
-  // await yUSDA.deployed();
-  // 0xca529bd64d95d56615e3aecbf3aeb1be899a5090
-  // const yUSDB = await StandardTokenTemplate.deploy();
-  // await yUSDB.deployed();
-  // 0x8bf346384ae2232077ccb596c48b8b934aa4177a
-  // const yUSDC = await StandardTokenTemplate.deploy();
-  // await yUSDC.deployed();
-  // await yUSDA.initialize('0x2e6bE9855A3bF02C73Ba74B7d756a63DB7762238', '1', 'yUSDA', 'yUSDA', 18);
-  // await yUSDB.initialize('0x2e6bE9855A3bF02C73Ba74B7d756a63DB7762238', '1', 'yUSDB', 'yUSDB', 18);
-  // await yUSDC.initialize('0x2e6bE9855A3bF02C73Ba74B7d756a63DB7762238', '1', 'yUSDC', 'yUSDC', 18);
+  const yUSDA = await StandardTokenTemplate.deploy();
+  await yUSDA.deployed();
+  const yUSDB = await StandardTokenTemplate.deploy();
+  await yUSDB.deployed();
+  const yUSDC = await StandardTokenTemplate.deploy();
+  await yUSDC.deployed();
+  await yUSDA.deployed();
+  await yUSDA.initialize('0x2e6bE9855A3bF02C73Ba74B7d756a63DB7762238', '1', 'yUSDA', 'yUSDA', 18);
+  await yUSDB.deployed();
+  await yUSDB.initialize('0x2e6bE9855A3bF02C73Ba74B7d756a63DB7762238', '1', 'yUSDB', 'yUSDB', 18);
+  await yUSDC.deployed();
+  await yUSDC.initialize('0x2e6bE9855A3bF02C73Ba74B7d756a63DB7762238', '1', 'yUSDC', 'yUSDC', 18);
 
   // 2. SCAMM 배포
   const scamm = await SCAMMTemplate.deploy(
     '0x2e6bE9855A3bF02C73Ba74B7d756a63DB7762238',
     scaleToken.address,
-    [
-      '0x2f2d1790ae082d1847e9c4eefb58bd311f6ff886',
-      '0xca529bd64d95d56615e3aecbf3aeb1be899a5090',
-      '0x8bf346384ae2232077ccb596c48b8b934aa4177a',
-    ],
+    [yUSDA.address, yUSDB.address, yUSDC.address],
     50, // 0.5%
   );
   await scamm.deployed();
   await scaleToken.initialize(scamm.address, '1', 'uUSD', 'uUSD', 18);
 
-  console.log('yUSDA deployed to:', '0x2f2d1790ae082d1847e9c4eefb58bd311f6ff886');
-  console.log('yUSDB deployed to:', '0xca529bd64d95d56615e3aecbf3aeb1be899a5090');
-  console.log('yUSDC deployed to:', '0x8bf346384ae2232077ccb596c48b8b934aa4177a');
+  console.log('yUSDA deployed to:', yUSDA.address);
+  console.log('yUSDB deployed to:', yUSDB.address);
+  console.log('yUSDC deployed to:', yUSDC.address);
   console.log('SCAMM deployed to:', scamm.address);
   console.log('ScaleToken deployed to:', scaleToken.address);
 }
